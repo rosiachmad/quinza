563640f3b50216f64852f4356e2680723a85cd16
rosiachmad <ealbri4@my.wgu.edu>
2018-03-22 21:53:39 +0700

    Update manggil.js

--- manggil.js
+++ manggil.js
@@ -22,10 +22,11 @@ if(a_to_vh==false)
 var x=Math.floor((Math.random()*5)+ 1);
 var xxx=null;
 if(x=="1"){xxx="/2018/03/post-1.html"}
-if(x=="2"){xxx="/2018/03/post-1.html"}
-if(x=="3"){xxx="/2018/03/post-1.html"}
-if(x=="4"){xxx="/2018/03/post-1.html"}
-if(x=="5"){xxx="/2018/03/post-1.html"}a_to_ve[i].href="https://quinza-safelink.blogspot.com"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="2"){xxx="/2018/03/post-2.html"}
+if(x=="3"){xxx="/2018/03/post-3.html"}
+if(x=="4"){xxx="/2018/03/post-4.html"}
+if(x=="5"){xxx="/2018/03/post-5.html"}
+if(x=="6"){xxx="/2018/03/post-6.html"}a_to_ve[i].href="https://quinza-safelink.blogspot.com"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
